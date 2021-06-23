63dcb1f60b23ab4a24dd06ded40162d9653eca07
Janis Feldmanis <feldmanis45@gmail.com>
2021-06-23 15:11:15 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function quest_73908_features () {
+    let actionAnims = {};
+    let scrollTriggersFired = [];
+    let fireScrollOffTriggers = [];
+    let parallaxScrollElements = [];
+  
+  };
+  
+      document.addEventListener("DOMContentLoaded", quest_73908_features);
+    0
+0
